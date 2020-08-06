95dbdd19d1f02573ef1ab8d53cc29053b26b9e21
Yonatanmulatu <yonatan.t.mulatu@gmail.com>
2020-08-06 08:42:36 +0800

    Updates

---
+++ precache-manifest.b3980ad59e1f6a40055eb0ef03453a22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc39e6f382f07a4784e02715ac798632",
+    "url": "/calculatorapp/index.html"
+  },
+  {
+    "revision": "57ae825dd1c3c5c79f11",
+    "url": "/calculatorapp/static/css/main.01102b2d.chunk.css"
+  },
+  {
+    "revision": "49473d090a8504d8e7a0",
+    "url": "/calculatorapp/static/js/2.ad7dacbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculatorapp/static/js/2.ad7dacbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ae825dd1c3c5c79f11",
+    "url": "/calculatorapp/static/js/main.7dffc237.chunk.js"
+  },
+  {
+    "revision": "1200319bd998099bf6ff",
+    "url": "/calculatorapp/static/js/runtime-main.8ffd93dc.js"
+  }
+]);
